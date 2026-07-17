a34b780efa0dbc7a7c92c0758db6fa64396d9891
katorly <i@katorly.com>
2026-07-17 17:55:39 +0800

    feat(core/slider): add slider component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./components/button/oreui-button.js";
 export * from "./components/checkbox/oreui-checkbox.js";
 export * from "./components/input/oreui-input.js";
+export * from "./components/slider/oreui-slider.js";
 export * from "./components/switch/oreui-switch.js";
