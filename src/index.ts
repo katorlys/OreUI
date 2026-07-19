763a5848c122e8a230ccf1cbd177e9a942be7162
katorly <i@katorly.com>
2026-07-19 16:48:46 +0800

    feat(core/icon): add 27 icons and 47 flags
    - currently all of them are from the Figma design file mentioned in README.md

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "./components/button/oreui-button.js";
 export * from "./components/checkbox/oreui-checkbox.js";
+export * from "./components/icon/icon-list.js";
+export * from "./components/icon/oreui-icon.js";
 export * from "./components/input/oreui-input.js";
 export * from "./components/slider/oreui-slider.js";
 export * from "./components/switch/oreui-switch.js";
